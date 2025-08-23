edf4044680d7c8673f2c449a01f2d91017792004
ritesh9764 <riteshfalke01@gmail.com>
2025-08-23 16:30:14 +0530

    frist commit

---
+++ Assignment1/MeanCalculater.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+	public class MeanCalculater{
+	public static void main(String[] in){
+
+	double a,b;
+
+	Scanner sc = new Scanner(System.in);
+
+	System.out.println("enter frist number :");
+	a = sc.nextDouble();
+
+	System.out.println("enter second number :");
+	b  = sc.nextDouble();
+
+	double am = (a+b)/2;
+	
+	double hm =(2 * a *b) /(a+b);
+
+	System.out.println("Arithmetic mean :"+am);
+	System.out.println("Harmonic mean :"+hm);
+}
+}
+
