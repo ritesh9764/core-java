9a8b72c0f96949788949a9bb4870a75206835827
ritesh9764 <riteshfalke01@gmail.com>
2025-08-23 16:27:52 +0530

    commit

---
+++ Assignment1/MotionCalculater.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class MotionCalculater {
+
+    public static void main(String[] args) {
+
+        double u, a, t;  // Declared inside main method, accessible throughout main
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter initial velocity (u):");
+        u = sc.nextDouble();
+
+        System.out.println("Enter acceleration (a):");
+        a = sc.nextDouble();
+
+        System.out.println("Enter time (t):");
+        t = sc.nextDouble();
+
+        double v = u + a * t;                     	
+        double s = u * t + 0.5 * a * t * t;       
+
+        System.out.println("Final velocity (v): " + v);
+        System.out.println("Distance traveled (s): " + s);
+
+        sc.close();
+    }
+}
