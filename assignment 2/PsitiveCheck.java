42f21c9641606d1a9cb886ff2d5e5adb01290962
ritesh9764 <riteshfalke01@gmail.com>
2025-08-27 15:48:35 +0530

    frist commit

---
+++ assignment 2/PsitiveCheck.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+	public class PsitiveCheck{
+
+	public static void main(String[] args){
+
+	Scanner sc = new Scanner(System.in);
+
+	int num;
+	
+	System.out.println("Enter a number :");
+	num = sc.nextInt();
+	
+	if (num > 0){
+	     System.out.println("positive n :"+num);
+	} else {
+	     System.out.println("not positive n :"+num);
+	}
+}
+}
