55ea98608a3038d432d600279a3252c209e08c61
ritesh9764 <riteshfalke01@gmail.com>
2025-08-27 17:44:36 +0530

    frist commit

---
+++ assignment 2/CompareNumbers.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class CompareNumbers {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter first number: ");
+        int num1 = sc.nextInt();
+
+        System.out.print("Enter second number: ");
+        int num2 = sc.nextInt();
+
+        // Comparison using relational operators
+        if (num1 == num2) {
+            System.out.println(num1 + " is EQUAL to " + num2);
+        } else if (num1 > num2) {
+            System.out.println(num1 + " is GREATER than " + num2);
+        } else {
+            System.out.println(num1 + " is LESS than " + num2);
+        }
+
+        sc.close();
+    }
+}
