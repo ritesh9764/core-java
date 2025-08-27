a1fec3cda8a50782eefafddc832446b2d8948e27
ritesh9764 <riteshfalke01@gmail.com>
2025-08-27 15:54:11 +0530

    frist commit

---
+++ assignment 2/PositiveNegativeCheck.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class PositiveNegativeCheck {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter a number: ");
+        int num = scanner.nextInt();
+
+        if (num > 0) {
+            System.out.println(num + " is positive.");
+        } else if (num < 0) {
+            System.out.println(num + " is negative.");
+        } else {
+            System.out.println(num + " is zero.");
+        }
+
+        scanner.close();
+    }
+}
