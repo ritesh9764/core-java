a4f1358c24fa7f4f8999206b447fc98f561ec1a7
ritesh9764 <riteshfalke01@gmail.com>
2025-08-27 16:10:16 +0530

    frist commit

---
+++ assignment 2/ProfitLossCalculator.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class ProfitLossCalculator {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter the Cost Price: ");
+        double costPrice = scanner.nextDouble();
+
+        System.out.print("Enter the Selling Price: ");
+        double sellingPrice = scanner.nextDouble();
+
+        if (sellingPrice > costPrice) {
+            double profit = sellingPrice - costPrice;
+            System.out.println("Profit: " + profit);
+        } else if (sellingPrice < costPrice) {
+            double loss = costPrice - sellingPrice;
+            System.out.println("Loss: " + loss);
+        } else {
+            System.out.println("No Profit No Loss");
+        }
+
+        scanner.close();
+    }
+}
