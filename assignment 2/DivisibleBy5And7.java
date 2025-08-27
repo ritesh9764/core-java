933242e10322f25439d414d4d7bd6d8505c103d7
ritesh9764 <riteshfalke01@gmail.com>
2025-08-27 16:12:26 +0530

    frist commit

---
+++ assignment 2/DivisibleBy5And7.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class DivisibleBy5And7 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a number: ");
+        int num = sc.nextInt();
+
+        if (num % 5 == 0 && num % 7 == 0) {
+            System.out.println(num + " is divisible by both 5 and 7.");
+        } else {
+            System.out.println(num + " is NOT divisible by both 5 and 7.");
+        }
+
+        sc.close();
+    }
+}
