6a71e171366b78ff0e0ad8b0a3f1059f4c2ee892
ritesh9764 <riteshfalke01@gmail.com>
2025-08-27 23:50:41 +0530

    frist commit

---
+++ assignment 2/DivisibleBy5Or7.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+	public class DivisibleBy5Or7 {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a number: ");
+        int num = sc.nextInt();
+
+        
+        if (num % 5 == 0 || num % 7 == 0) {
+            System.out.println(num + " is divisible by 5 or 7");
+        } else {
+            System.out.println(num + " is NOT divisible by 5 or 7");
+        }
+
+        
+    }
+}
