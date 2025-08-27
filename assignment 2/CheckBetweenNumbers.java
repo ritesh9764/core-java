8814a4c68aad7c6939e000f6719f6439d936d24e
ritesh9764 <riteshfalke01@gmail.com>
2025-08-27 16:27:48 +0530

    frist commit

---
+++ assignment 2/CheckBetweenNumbers.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class CheckBetweenNumbers {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter first number: ");
+        int first = sc.nextInt();
+
+        System.out.print("Enter second number: ");
+        int second = sc.nextInt();
+
+        System.out.print("Enter third number: ");
+        int third = sc.nextInt();
+
+        // Check if first is between second and third (inclusive)
+        if ((first >= second && first <= third) || (first >= third && first <= second)) {
+            System.out.println(first + " is between " + second + " and " + third);
+        } else {
+            System.out.println(first + " is NOT between " + second + " and " + third);
+        }
+
+        sc.close();
+    }
+}
