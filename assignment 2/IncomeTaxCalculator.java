a43fab9c73385ad48f0412036018337a364a80a4
ritesh9764 <riteshfalke01@gmail.com>
2025-09-01 21:25:07 +0530

    frist commit

--- assignment 2/IncomeTaxCalculator.java
+++ assignment 2/IncomeTaxCalculator.java
@@ -1,24 +1,18 @@
 import java.util.Scanner;
-
 public class IncomeTaxCalculator {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-
         System.out.print("Enter annual basic salary: ");
         double salary = sc.nextDouble();
-
-        double tax = 0;
+        double tax;
 
         if (salary < 150000) {
             tax = 0;
-        } else if (salary >= 150000 && salary <= 300000) {
-            tax = 0.20 * salary;
-        } else if (salary > 300000) {
-            tax = 0.30 * salary;
+        } else if (salary <= 300000) {
+            tax = salary * 0.20; 
+        } else {
+            tax = salary * 0.30; 
         }
-
-        System.out.println("Income tax to be paid: Rs. " + tax);
-
-        sc.close();
+        System.out.println("Income Tax = ₹" + tax);
     }
 }
