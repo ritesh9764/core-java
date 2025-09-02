bedcac660063e513fde7d1775ac00dfd7e16e748
ritesh9764 <riteshfalke01@gmail.com>
2025-09-02 19:39:30 +0530

    frist commit

--- assignment 2/LibraryFine.java
+++ assignment 2/LibraryFine.java
@@ -1,27 +1,33 @@
 import java.util.Scanner;
 
-	public class LibraryFine{
+public class LibraryFine {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
 
-	public static void main(String[] args){
 
-		Scanner sc=new Scanner(System.in);
+        System.out.print("Enter number of days book is returned late: ");
+        int daysLate = scanner.nextInt();
 
-		System.out.println("Enter number of Days late");
-		int days=sc.nextInt();
+        int fine = 0;
 
-		int fine;
-		if( days < 5)
-		{
-			fine= 1 * days;
-		}
-		else if(days <= 10)
-		{
-			fine=5 * days;
-		}
-		else
-		{
-			fine=10 * days;
-		}
-			System.out.println("Fine = Rs." +fine);
-	}
+        if (daysLate > 0 && daysLate < 5)
+        {
+            fine = daysLate * 5;     
+        }
+         else if (daysLate >= 6 && daysLate <= 10)
+        {
+            fine = daysLate * 10;    
+        } 
+        else if (daysLate > 10) 
+        {
+            fine = daysLate * 15;    
+        } else 
+        {
+            fine = 0; 
+        }
+
+        
+        System.out.println("Fine to be paid: Rs " + fine);
+
+       }
 }
