654217617caee12ea68c3c9acbd79ab0d8502912
ritesh9764 <riteshfalke01@gmail.com>
2025-09-03 10:22:34 +0530

    frist commit

---
+++ assignment3/Calculator.java
@@ -0,0 +1,46 @@
+import java.util.Scanner;
+
+public class Calculator {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter first integer: ");
+        int num1 = sc.nextInt();
+
+        System.out.print("Enter second integer: ");
+        int num2 = sc.nextInt();
+
+        System.out.print("Enter operator (+, -, *, /): ");
+        char operator = sc.next().charAt(0);
+
+        int result = 0;
+        boolean valid = true;
+
+        switch (operator) {
+            case '+':
+                result = num1 + num2;
+                break;
+            case '-':
+                result = num1 - num2;
+                break;
+            case '*':
+                result = num1 * num2;
+                break;
+            case '/':
+                if (num2 != 0) {
+                    result = num1 / num2;
+                } else {
+                    System.out.println("Error: Division by zero is not allowed.");
+                    valid = false;
+                }
+                break;
+            default:
+                System.out.println("Invalid operator.");
+                valid = false;
+        }
+
+        if (valid) {
+            System.out.println("Result: " + result);
+        }
+    }
+}
